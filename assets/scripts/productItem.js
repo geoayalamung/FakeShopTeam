ae527abe63c2536b974ecad6f809e9d81caecb31
anakarenmugon <anakarenmugon07@gmail.com>
2023-05-23 16:50:53 -0700

    step two

--- assets/scripts/productItem.js
+++ assets/scripts/productItem.js
@@ -1,1 +1,37 @@
 //Contiene la definición de la clase "ProductItem".
+/* ### Step 2 - Product Item
+
+Create a class called `ProductItem` that will have the following properties:
+ */
+/* ```js
+const productItem = {
+  product: {}, // instance of Product class
+  addToCart() {
+    // just console.log the product for now
+  },
+  render() {
+    // return the product element with the product data (<li>)
+  },
+};
+``` */
+
+/* Test your class by creating a new instance of it with hardcoded values and calling the `render` method. Also, test the `addToCart` method by clicking on the button and check if shows the product in the console. */
+
+class ProductItem{
+    constructor(product){
+        this.product = product;
+    }
+    addToCart(){
+        shoppingCart.addProduct(this.product);
+        console.log(this.product);
+    }
+    render(){
+        const li = document.createElement('li');
+        list.innerHTML = ` img src = "${this.product.image}" alt = "${this.product.tittle}"/>
+        <h3>${this.product.tittle}</h3>
+        <p>Price: $$this.product.price}</p>
+        <p>${this.product.description}</p>
+        <button onclick = "addToCart(this.id)">Add to cart</button>`;
+        return li;
+    }
+}
--- assets/scripts/productList.js
+++ assets/scripts/productList.js
@@ -1,1 +1,45 @@
 //Contiene la definición de la clase "ProductList".
+/* ### Step 3 - Product List
+
+Create a class called `ProductList` that will have the following properties:
+
+```js
+const productList = {
+  products: [],
+  fetchProducts: function () {
+    // fetch the products from the API
+  },
+  render() {
+    // render the products by looping through the products array and create a new ProductItem instance for each product. Use render method of ProductItem class to get each product element and append it to the `<ul>`.
+  },
+};
+```
+
+> API to be used: https://fakestoreapi.com/products */
+
+class ProductList{
+    constructor(){
+        this.products = [];
+
+}
+async fetchProducts(){
+    try{
+        const response = await fetch('https://fakestoreapi.com/products');
+        const data = await response.json();
+        this.products = data.map((item) =>{
+            return new Product(item.id, item.title, item.price);
+        });
+        this.render();
+        }catch(error){
+            console.log(error);
+    }
+}
+render(){
+    const ul = document.createElement('ul');
+    this.products.forEach((product) => {
+        const productItem =new ProductItem(product);
+        ul.appendChild(productItem.render());
+});
+document.body.appendChild(ul);
+}
+}
