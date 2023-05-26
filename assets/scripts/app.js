4d72f39e01044158dd2b84b8f47172c75ef45cb0
anakarenmugon <anakarenmugon07@gmail.com>
2023-05-26 13:30:00 -0700

    file app

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,55 @@
 //Contiene la definición de la clase App y es el punto de entrada de la aplicación.
+
+const cart = [];
+
+var products ={
+  items: [
+    {
+      id: 1,
+      name: "A Pillow",
+      price: 19.99,
+      description: "A soft pillow!",
+    },
+    {
+      id: 2,
+      name: "A Carpet",
+      price: 89.99,
+      description: "A carpet which you might like - or not.",
+    },
+   /* Creo que debemos agregar los productos de las APIS */
+  ],
+
+  addToCart: function (productId) {
+    var product = this.items.find(function(item) {
+      return item.id === productId;
+    });
+
+    if (product) {
+      cart.push(product);
+      console.log("Product added to your cart:", product);
+    }
+  },
+};
+
+var addToCartButtons = document.querySelectorAll(".product-item button");
+
+
+addToCartButtons.forEach(function (button) {
+  button.addEventListener("click", function (event) {
+    var productId = parseInt(
+      event.target.closest(".product-item").dataset.productId
+    );
+    products.addToCart(productId);
+  });
+});
+
+
+function orderNow() {
+    console.log("¡Order Made!");
+    console.log("Products in the Cart:", cart);
+  }
+  
+  var orderButton = document.querySelector(".cart button");
+  
+  orderButton.addEventListener("click", orderNow);
+  
