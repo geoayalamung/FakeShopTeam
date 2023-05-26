7d7d62dbbf4cec997c27e2cf2d949db469fdaeaa
anakarenmugon <anakarenmugon07@gmail.com>
2023-05-26 13:25:12 -0700

    In the inspection if recognize

--- assets/scripts/shop.js
+++ assets/scripts/shop.js
@@ -1,1 +1,22 @@
 //Contiene la definición de la clase "Shop"
+
+
+
+
+/* import Product from './product.js';
+import ShoppingCart from './shoppingCart.js';
+import ProductList from './productList.js';
+
+class Shop {
+  render() {
+    const productList = new ProductList();
+    productList.fetchProducts();
+
+    const shoppingCart = new ShoppingCart();
+    shoppingCart.render();
+  }
+}
+
+const shop = new Shop();
+shop.render();
+ */
--- assets/scripts/shoppingCart.js
+++ assets/scripts/shoppingCart.js
@@ -1,1 +1,21 @@
 // Contiene la definición de la clase "ShoppingCart".
+
+
+
+
+/* class ShoppingCart {
+    constructor() {
+      this.cart = [];
+    }
+  
+    addToCart(product) {
+      this.cart.push(product);
+      console.log("Product added to your cart:", product);
+    }
+  
+    render() {
+    }
+  }
+  
+  export default ShoppingCart; */
+  
